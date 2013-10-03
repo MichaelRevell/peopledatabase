8ff5ca2c89b00d16b1f51983e83a9abe2d8988d2
Michael Revell <mikearevell@gmail.com>
2013-10-02 23:59:37 -0700

    Initial Commit

---
+++ PeopleDatabase/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PeopleDatabase
+//
+//  Created by Michael Revell on 10/2/13.
+//  Copyright (c) 2013 Michael Revell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
